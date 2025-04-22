21fde6acfde93cc4f06b5e0bc0d912ecd0fe244b
ShawnWx2009 <shawnwang2016@126.com>
2025-04-22 22:46:40 +0800

    project init

---
+++ R/data.R
@@ -0,0 +1,42 @@
+#' @title MoNA - MassBank of North America database
+#' @description MassBank of North America database
+#' @author Xiaotao Shen
+#' \email{xiaotao.shen@@outlook.com}
+#' @docType data
+#' @name mona_ms2
+#' @format A databaseClass object.
+
+"mona_ms2"
+
+##------------------------------------------------------------------------------
+#' @title massbank database database
+#' @description massbank database database
+#' @author Xiaotao Shen
+#' \email{xiaotao.shen@@outlook.com}
+#' @docType data
+#' @name massbank_ms2
+#' @format A databaseClass object.
+
+"massbank_ms2"
+
+##------------------------------------------------------------------------------
+#' @title Human Metabolome Database
+#' @description HMDB
+#' @author Xiaotao Shen
+#' \email{xiaotao.shen@@outlook.com}
+#' @docType data
+#' @name hmdb_ms2
+#' @format A databaseClass object.
+
+"hmdb_ms2"
+
+##------------------------------------------------------------------------------
+#' @title Global Natural Product Social Molecular Networking
+#' @description GNPS
+#' @author Xiaotao Shen
+#' \email{xiaotao.shen@@outlook.com}
+#' @docType data
+#' @name gnps_ms2
+#' @format A databaseClass object.
+
+"gnps_ms2"
